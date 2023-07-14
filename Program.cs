6af694f7e143c2f2fadb87765a4cdf29e31f3494
aryark <aryark3112@gmail.com>
2023-07-14 10:17:41 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -8,35 +8,75 @@ namespace _14july2023
         static void Main(string[] args)
         {
             //Jagged Array
-            int noc; //number of class
-            int nos; //number of student
-            Console.WriteLine("Enter Number of Classes");
-            noc = int.Parse(Console.ReadLine());
-            string[][] students = new string[noc][];
-            for (int i = 0; i < noc; i++)
+            //int noc, nos;             //noc=number of class; nos=number of student
+            //Console.WriteLine("Enter Number of Classes");
+            //noc = int.Parse(Console.ReadLine());
+            //string[][] students = new string[noc][];
+            //for (int i = 0; i < noc; i++)
+            //{
+            //    Console.WriteLine("Enter Number of Student in Class " + (i + 1));
+            //    nos = int.Parse(Console.ReadLine());
+
+            //    students[i] = new string[nos];
+            //    for (int j = 0; j < nos; j++)
+            //    {
+            //        Console.WriteLine($"Enter Student {j + 1}\'s name");
+            //        students[i][j] = Console.ReadLine();
+            //    }
+            //}
+            //for (int i = 0; i < students.Length; i++)
+            //{
+            //    Console.WriteLine($" Students List of class {i + 1}");
+            //    Console.WriteLine("************************");
+            //    for (int j = 0; j < students[i].Length; j++)
+            //    {
+            //        Console.WriteLine(students[i][j]);
+            //    }
+            //    Console.WriteLine("************************");
+            //}
+            //Console.ReadKey();
+
+            //Jagged Array Exercise-01
+            Console.Write("Enter the number of employees: ");
+            int numEmployees = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the number of working days for each employee: ");
+            int numDays = Convert.ToInt32(Console.ReadLine());
+
+            // Create a jagged array to store salaries
+            int[][] salaries = new int[numEmployees][];
+
+            // Input salaries for each employee
+            for (int i = 0; i < numEmployees; i++)
             {
-                Console.WriteLine("Enter Number of Student in Class " + (i + 1));
-                nos = int.Parse(Console.ReadLine());
-                
-                students[i] = new string[nos];
-                for(int j=0; j<nos; j++)
+                salaries[i] = new int[numDays];
+
+                for (int j = 0; j < numDays; j++)
                 {
-                    Console.WriteLine($"Enter Student {j + 1}\'s name");
-                    students[i][j] = Console.ReadLine();
+                    Console.Write($"Enter salary for Employee {i + 1}, Day {j + 1}: ");
+                    salaries[i][j] = Convert.ToInt32(Console.ReadLine());
                 }
             }
-            for(int i=0; i<students.Length; i++)
+
+            // Display the salary for each day and calculate the total salary for each employee
+            Console.WriteLine("\nDay1\tDay2\tDay3\tDay4\tDay5\tTotalSalary");
+
+            for (int i = 0; i < numEmployees; i++)
             {
-                Console.WriteLine($" Students List of class {i + 1}");
-                Console.WriteLine("************************");
-                for (int j = 0; j < students[i].Length; j++)
+                int totalSalary = 0;
+
+                for (int j = 0; j < numDays; j++)
                 {
-                    Console.WriteLine(students[i][j]);
+                    Console.Write($"{salaries[i][j]}\t");
+                    totalSalary += salaries[i][j];
                 }
-                Console.WriteLine("************************");
+
+                Console.WriteLine(totalSalary);
             }
+
             Console.ReadKey();
-                
+
+
         }
     }
 }
